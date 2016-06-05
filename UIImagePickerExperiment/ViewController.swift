d0384ca6d80ce2ca7d94854b46a6499fd5236ed9
yuanjun221 <yuanjun221@gmail.com>
2016-06-05 15:23:57 +0800

    complete layout

--- UIImagePickerExperiment/ViewController.swift
+++ UIImagePickerExperiment/ViewController.swift
@@ -8,16 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imagePickerView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
+    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func pickAnImageFromAlbum(sender: AnyObject) {
+        presentImagePickerControllerWithSourceType(.PhotoLibrary)
+    }
+    
+    
+    @IBAction func pickAnImageFromCamera(sender: AnyObject) {
+        presentImagePickerControllerWithSourceType(.Camera)
+    }
+    
+    
+    func presentImagePickerControllerWithSourceType(type: UIImagePickerControllerSourceType) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = type
+        presentViewController(pickerController, animated: true, completion: nil)
+    }
+    
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imagePickerView.image = image
+        }
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
     }
 
 
