e0b3ac365d1d3b9436a4d464ec3d78fab2ca93d7
yuanjun221 <yuanjun221@gmail.com>
2016-06-06 17:20:44 +0800

    complete with V1.0

---
+++ UIImagePickerExperiment/MemeStruct.swift
@@ -0,0 +1,17 @@
+//
+//  MemeStruct.swift
+//  UIImagePickerExperiment
+//
+//  Created by Jun.Yuan on 16/6/6.
+//  Copyright © 2016年 Jun.Yuan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var topText: String
+    var bottomText: String
+    var originImage: UIImage
+    var memedImage: UIImage
+}
--- UIImagePickerExperiment/ViewController.swift
+++ UIImagePickerExperiment/ViewController.swift
@@ -11,54 +11,31 @@ import UIKit
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
     // MARK: -
-    // MARK: variables
+    // MARK: Variables
     @IBOutlet weak var imagePickerView: UIImageView!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     @IBOutlet weak var topTextField: UITextField!
     @IBOutlet weak var bottomTextField: UITextField!
     @IBOutlet weak var shareButton: UIBarButtonItem!
+    @IBOutlet weak var resetButton: UIBarButtonItem!
+    @IBOutlet weak var navigationBar: UINavigationBar!
+    @IBOutlet weak var bottomToolbar: UIToolbar!
     
     private var memedImage: UIImage!
     
-    struct Meme {
-        var topText: String
-        var bottomText: String
-        var originImage: UIImage
-        var memedImage: UIImage
-    }
     
-    
-    func save() -> Meme {
+    // MARK: -
+    // MARK: Save Meme object
+    func saveMeme() {
         //Create the meme
-        let meme = Meme(topText: topTextField.text!,
+        let _ = Meme(topText: topTextField.text!,
                         bottomText: bottomTextField.text!,
                         originImage: imagePickerView.image!,
                         memedImage: memedImage)
-        return meme
     }
-    
-    
-    func generateMemedImage() -> UIImage {
-        
-        // Render view to an image
-        UIGraphicsBeginImageContext(self.view.frame.size)
-        view.drawViewHierarchyInRect(self.view.frame, afterScreenUpdates: true)
-        let memedImage : UIImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        
-        return memedImage
-    }
-    
     
     // MARK: -
-    // MARK: view life cycle
-    func setTextField(textfield: UITextField, withDelegate delegate: UITextFieldDelegate, textAttribute: [String: AnyObject], aligment: NSTextAlignment) {
-        textfield.delegate = delegate
-        textfield.defaultTextAttributes = textAttribute
-        textfield.textAlignment = aligment
-    }
-    
-    
+    // MARK: View lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -71,6 +48,12 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         setTextField(topTextField, withDelegate: self, textAttribute: memeTextAttributes, aligment: .Center)
         setTextField(bottomTextField, withDelegate: self, textAttribute: memeTextAttributes, aligment: .Center)
+    }
+    
+    func setTextField(textfield: UITextField, withDelegate delegate: UITextFieldDelegate, textAttribute: [String: AnyObject], aligment: NSTextAlignment) {
+        textfield.delegate = delegate
+        textfield.defaultTextAttributes = textAttribute
+        textfield.textAlignment = aligment
     }
     
     
@@ -86,7 +69,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         subscribeToKeyboardNotifications()
     }
     
-    
+
     override func viewWillDisappear(animated: Bool) {
         super.viewWillDisappear(animated)
         unsubscribeFromKeyboardNotifications()
@@ -99,7 +82,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     
     // MARK: -
-    // MARK: button actions
+    // MARK: Button actions
     @IBAction func pickAnImageFromAlbum(sender: AnyObject) {
         presentImagePickerControllerWithSourceType(.PhotoLibrary)
     }
@@ -116,7 +99,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         activityViewController.completionWithItemsHandler = {
             activityType, completed, returnedItems, activityError in
             if completed {
-                self.save()
+                self.saveMeme()
                 self.dismissViewControllerAnimated(true, completion: nil)
             }
         }
@@ -124,9 +107,43 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     
+    func generateMemedImage() -> UIImage {
+        navigationBar.hidden = true
+        bottomToolbar.hidden = true
+        // Render view to an image
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawViewHierarchyInRect(self.view.frame, afterScreenUpdates: true)
+        let memedImage : UIImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        navigationBar.hidden = false
+        bottomToolbar.hidden = false
+        return memedImage
+    }
+    
+    
+    @IBAction func resetEditor(sender: AnyObject) {
+        let alertController = UIAlertController(title: "Reset Editor", message: "Changes unsaved will be lost", preferredStyle: .ActionSheet)
+        
+        let resetAction = UIAlertAction(title:"Reset", style: .Destructive) { action in
+            self.topTextField.text = "TOP"
+            self.bottomTextField.text = "BOTTOM"
+            self.imagePickerView.image = nil
+            self.shareButton.enabled = false
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }
+        let cancelAction = UIAlertAction(title:"Cancel", style: .Cancel) { action in
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }
+        alertController.addAction(resetAction)
+        alertController.addAction(cancelAction)
+        presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    
     
     // MARK: -
-    // MARK: image picker delegate
+    // MARK: Image picker delegate
     func presentImagePickerControllerWithSourceType(type: UIImagePickerControllerSourceType) {
         let pickerController = UIImagePickerController()
         pickerController.delegate = self
@@ -149,7 +166,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     
     // MARK: -
-    // MARK: text field delegate
+    // MARK: Text field delegate
     func textFieldDidBeginEditing(textField: UITextField) {
         let text = textField.text
         if text == "TOP" || text == "BOTTOM" {
@@ -180,7 +197,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     
     // MARK: -
-    // MARK: toggle view when editing bottom textField
+    // MARK: Toggle view when editing bottom textField
     func getKeyboardHeight(notification: NSNotification) -> CGFloat {
         let userInfo = notification.userInfo
         let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue // of CGRect
